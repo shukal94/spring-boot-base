19901872e635d8bf076847a032f02de7ebac39a4
sshukalovich <sshukalovich@cleargov.com>
2023-11-22 17:12:34 +0100

    logback is loaded by default, take a look at spring dependencies first before include anythinggit add .

--- src/main/java/ch/shukalovi/tranining/spring/springbootclassic/SpringBootClassicApplication.java
+++ src/main/java/ch/shukalovi/tranining/spring/springbootclassic/SpringBootClassicApplication.java
@@ -1,12 +1,15 @@
 package ch.shukalovi.tranining.spring.springbootclassic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class SpringBootClassicApplication {
 
 	public static void main(String[] args) {
+		log.info("HELLO!!!");
 		SpringApplication.run(SpringBootClassicApplication.class, args);
 	}
 
