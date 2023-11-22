bc93cabbbfe9d50924857dbf1ad4864e4bf77e84
sshukalovich <sshukalovich@cleargov.com>
2023-11-22 17:07:02 +0100

    initial spring boot structure

---
+++ src/main/java/ch/shukalovi/tranining/spring/springbootclassic/SpringBootClassicApplication.java
@@ -0,0 +1,13 @@
+package ch.shukalovi.tranining.spring.springbootclassic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootClassicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootClassicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/shukalovi/tranining/spring/springbootclassic/SpringBootClassicApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.shukalovi.tranining.spring.springbootclassic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootClassicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
